25cf1bcd37bb1943c40b9fa36a26151fe8f841a0
Zane LI <zhanzhan12821@gmail.com>
2025-04-09 23:47:09 +0800

    init flask

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev'  
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
