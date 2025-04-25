bd3f7c47a9bf86a5d1d09309825745fe8755778f
Resistnz <toby.fett@gmail.com>
2025-04-25 20:42:07 +0800

    Added new page to view shared stats

--- app/__init__.py
+++ app/__init__.py
@@ -33,13 +33,14 @@ def create_app():
     from .routes.stats import stats
     from .routes.share import share
     from .routes.gear import gear
+    from .routes.view_stats import view_stats
     
-
     app.register_blueprint(main)
     app.register_blueprint(auth)
     app.register_blueprint(stats)
     app.register_blueprint(share)
     app.register_blueprint(gear)
+    app.register_blueprint(view_stats)
 
     # Create database tables (only if not exist)
     with app.app_context():
